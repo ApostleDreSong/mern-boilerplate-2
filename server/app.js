b4713519b861ed1e3cdb4c0677e7bd32df3fc140
emg50 <emg50@duke.edu>
2018-04-18 16:53:47 -0400

    Add a directory structure to the server and edit scripts in the package.json

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import index from './routes/index';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/', index);
 
 app.listen(3000, () => console.log('listening on port 3000'));
