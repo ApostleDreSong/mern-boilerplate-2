7fae899ca8e2814ce30222be94d903c4fce4a2df
emg50 <emg50@duke.edu>
2018-04-18 14:41:43 -0400

    First commit

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+
+
+const Index = () => <div className="test">Hello React!</div>;
+
+ReactDOM.render(<Index />, document.getElementById('index'));
